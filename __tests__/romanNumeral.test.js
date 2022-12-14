d29fd737183ed3a079e8cbe2368840c89d9f9a0c
Carl Sustrich <carl.sustrich@gmail.com>
2022-12-13 18:58:12 -0800

    good fail

--- __tests__/romanNumeral.test.js
+++ __tests__/romanNumeral.test.js
@@ -1,7 +1,12 @@
 import RomanNumeral from './../src/romanNumeral.js';
 
 describe ('Roman Numeral', () => {
+
   test('it should return "I" when given a value of 1', () => {
     expect(RomanNumeral(1)).toEqual('I');
+  })
+
+  test('it should return "V" when given a value of 5', () => {
+    expect(RomanNumeral(5)).toEqual('V');
   })
 });
--- src/romanNumeral.js
+++ src/romanNumeral.js
@@ -1,5 +1,7 @@
 export default function romanNumeral(number) {
   if (number === 1) {
     return 'I';
-  }
+  } //else if (number === 5) {
+    //return 'V';
+  
 }
